209a91b5a9484a9b8c90a243c31f23104311758e
Eric-Okyere <ericokyere018@gmail.com>
2025-02-08 12:22:14 +0000

    Update

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -80,7 +80,7 @@ const About = () => {
  
     </div>
     
-    <div className='flex justify-between'>
+    <div className='flex justify-between mb-10'>
     <div className=' justify-center font-serif pt-6 '>
     {/* <h1 className=' md:text-2xl pt-6 mr-2'>@Linkpii</h1> */}
     <img src={Downl} className='w-80 md:h-18 lg:h-18 h-10 ' alt='down' />
