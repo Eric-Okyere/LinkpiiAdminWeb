130726b951b2f5a471490f13e19fa9e5c2e2663a
Eric-Okyere <ericokyere018@gmail.com>
2025-07-10 08:01:36 +0000

    Connection

--- src/assets/baseURL.js
+++ src/assets/baseURL.js
@@ -1,4 +1,4 @@
-const baseURL = "http://10.216.243.143:3000/"
+const baseURL = "https://linkpiii.onrender.com/"
 // http://192.168.43.26:3000/
 // https://palmfarmv.onrender.com/
 // http://192.168.43.94:3000/
